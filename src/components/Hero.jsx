c8d909041c53dcb67684bcde4714c8474d3c0397
Ross Gordon <rossagordonstl@gmail.com>
2023-04-22 12:23:00 -0400

    mobile viewport testing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,11 +14,11 @@ const Hero = () => {
       {/* The image container */}
       <div className="absolute inset-0 flex justify-center items-center md:justify-start">
         {/* The image */}
-        <div className="flex flex-col justify-center items-center md:absolute md:left-1/4 md:top-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 sm:left-1/2 sm:top-1/3">
+        <div className="flex flex-col justify-center items-center mt-[-330px] sm:mt-0 md:absolute md:left-1/4 md:top-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 sm:left-1/2 sm:top-1/4">
           <img
             src={svFront}
             alt="SV Front"
-            className="h-64 w-64 md:h-72 md:w-72 object-cover object-center scale-150"
+            className="h-64 w-64 md:h-72 md:w-72 object-cover object-center scale-75 md:scale-150 sm:h-32 sm:w-32 mb-4 md:mb-0"
           />
         </div>
       </div>
@@ -29,19 +29,20 @@ const Hero = () => {
         <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
         {/* The content */}
         <div className="absolute left-1/2 top-0 md:top-1/2 md:-translate-y-1/2 transform -translate-x-1/2 md:left-3/4 md:top-1/2 md:-translate-x-1/2 mt-[200px] sm:mt-0">
-  <div className="flex flex-col items-center">
-    <h1 className="font-bold text-white leading-tight text-center text-5xl md:text-7xl mt-8 sm:mt-0">
-      Skinny V Archive
-      <br />
-      Vol. 1
-    </h1>
-    <button className="mt-4 bg-white text-black border border-black py-3 px-6 rounded text-2xl hover:bg-black hover:text-white">
-      Listen Now
-    </button>
-  </div>
-</div>
-
-
+          {/* The text and button container */}
+          <div className="flex flex-col items-center md:absolute md:left-1/2 md:top-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 mt-12 md:mt-0">
+            {/* The text */}
+            <h1 className="font-bold text-white leading-tight text-center text-5xl md:text-7xl mt-8 sm:mt-0">
+              Skinny V Archive
+              <br />
+              Vol. 1
+            </h1>
+            {/* The button */}
+            <button className="mt-4 bg-white text-black border border-black py-3 px-6 rounded text-2xl hover:bg-black hover:text-white">
+              Listen Now
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
