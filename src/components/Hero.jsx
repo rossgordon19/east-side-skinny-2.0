da7cc106f30caea3c33cb049015d990be6bc2c21
Ross Gordon <rossagordonstl@gmail.com>
2023-04-22 19:05:52 -0400

    mobile testing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,37 +11,33 @@ const Hero = () => {
         style={{ backgroundImage: `url(${backgroundImage})` }}
       ></div>
 
-      {/* The image container */}
-      <div className="absolute inset-0 flex justify-center items-center md:justify-start">
-        {/* The image */}
-        <div className="flex flex-col justify-center items-center mt-[-300px] sm:mt-0 md:absolute md:left-1/4 md:top-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 sm:left-1/2 sm:top-1/4">
+      {/* The content */}
+      <div className="relative z-10 grid grid-cols-1 md:grid-cols-2 h-full px-4 sm:px-6 lg:px-8">
+        {/* The overlay */}
+        <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
+        
+        {/* The image container */}
+        <div className="flex justify-center items-center md:absolute md:left-1/4 md:top-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2">
+          {/* The image */}
           <img
             src={svFront}
             alt="SV Front"
-            className="h-72 w-72 md:h-72 md:w-72 object-cover object-center scale-75 md:scale-150 sm:h-42 sm:w-42 md:mb-0 sm:h-80 sm:w-80 sm:object-cover sm:object-center sm:scale-125"
+            className="h-72 w-72 md:h-72 md:w-72 object-cover object-center scale-75 sm:h-42 sm:w-42 md:mb-0 sm:h-80 sm:w-80 sm:object-cover sm:object-center scale-125"
           />
         </div>
-      </div>
 
-      {/* The content */}
-      <div className="relative z-10 flex flex-col justify-center items-center h-full px-4 sm:px-6 lg:px-8">
-        {/* The overlay */}
-        <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
         {/* The content */}
-        <div className="absolute left-1/2 top-0 md:top-1/2 md:-translate-y-1/2 transform -translate-x-1/2 md:left-3/4 md:top-1/2 md:-translate-x-1/2 mt-[200px] sm:mt-0">
-          {/* The text and button container */}
-          <div className="flex flex-col items-center md:absolute md:left-1/2 md:top-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 mt-12 md:mt-0">
-            {/* The text */}
-            <h1 className="font-bold text-white leading-tight text-center text-5xl md:text-7xl mt-[50px] sm:mt-0">
-              Skinny V Archive
-              <br />
-              Vol. 1
-            </h1>
-            {/* The button */}
-            <button className="mt-4 bg-white text-black border border-black py-3 px-6 rounded text-2xl hover:bg-black hover:text-white">
-              Listen Now
-            </button>
-          </div>
+        <div className="flex flex-col items-center justify-center md:absolute md:left-3/4 md:top-1/2 md:transform md:-translate-x-1/2 mb-[100px] lg:mt-[-145px]">
+          {/* The text */}
+          <h1 className="font-bold text-white text-center text-5xl md:text-7xl">
+            Skinny V Archive
+            <br />
+            Vol. 1
+          </h1>
+          {/* The button */}
+          <button className="mt-4 bg-white text-black border border-black py-3 px-6 rounded text-2xl hover:bg-black hover:text-white">
+            Listen Now
+          </button>
         </div>
       </div>
     </div>
