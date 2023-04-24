155ca54d59f12478858a68b05ea7c23b74f48f7a
Ross Gordon <rossagordonstl@gmail.com>
2023-04-24 11:44:41 -0400

    bg color updated for carousel componenet

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Carousel = () => {
   return (
-    <div className="bg-black text-white h-[80vh] w-full flex flex-col justify-center items-center">
+    <div className="bg-gray-400 h-[80vh] w-full flex flex-col justify-center items-center">
       <h1 className="text-5xl text-center">Carousel Goes Here</h1>
     </div>
   );
