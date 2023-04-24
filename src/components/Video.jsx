dbdc9c987e432cbf1cf84602c3607942d2da96eb
Ross Gordon <rossagordonstl@gmail.com>
2023-04-24 08:55:44 -0400

    video testing for mobile viewports

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import City from '../assets/building.mp4'
 import Sky from '../assets/sky.mp4';
 
 const Video = () => {
   return (
     <div className="relative h-[50vh] brightness-50 saturate-50 flex justify-center items-center">
-      <video className="absolute top-0 left-0 w-full h-full object-cover bg-video" autoPlay loop muted>
+      <video className="absolute top-0 left-0 w-full h-full bg-video object-cover sm:object-none" autoPlay loop muted>
         <source src={Sky} type="video/mp4" />
       </video>
-      <h1 className="text-white text-5xl font-bold text-center tracking-[0.85em] z-10">Text goes here</h1>
+      <h1 className="text-white text-5xl font-bold text-center tracking-[0.85em] z-10">Richmond, VA</h1>
     </div>
   )
 }
