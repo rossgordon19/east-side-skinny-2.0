f112c5b06c381c4664174479b2e1a828f596bd29
Ross Gordon <rossagordonstl@gmail.com>
2023-04-24 11:42:00 -0400

    Fixed height for Listen component

--- src/components/Listen.jsx
+++ src/components/Listen.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Listen = () => {
   return (
-    <div name='listen' className="bg-black text-white h-[80vh] w-full flex flex-col justify-center items-center">
+    <div name='listen' className="bg-black text-white h-[50vh] w-full flex flex-col justify-center items-center">
       <h1 className="text-5xl text-center">Listen</h1>
       <h1 className="mt-4">Streaming Links</h1>
     </div>
