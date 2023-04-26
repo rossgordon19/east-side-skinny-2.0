0616bcb88ce5da0034d38d2814e984d1749c5dbf
Ross Gordon <rossagordonstl@gmail.com>
2023-04-26 12:45:57 -0400

    viewport testing

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 import Sky from '../assets/sky.mp4';
 
 const Video = () => {
   return (
-    <div className="relative min-h-[50vh] brightness-50saturate-50 flex justify-center items-center text-center sm:text-left -mt-0.5">
-      <video className="absolute top-0 left-0 w-full h-full bg-video object-none border-none" autoPlay loop muted playsInline>
+    <div className="relative min-h-[50vh] brightness-50 saturate-50 flex justify-center items-center text-center sm:text-left -mt-0.5">
+      <video className="absolute top-0 left-0 w-full h-full object-cover border-none" autoPlay loop muted playsInline>
         <source src={Sky} type="video/mp4" />
       </video>
       <h1 className="text-white text-2xl md:text-4xl font-bold tracking-[0.85em] z-10">Richmond,VA</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
