8d07da29c0ee45cb5459f3ef7ced5104e13a9861
Ross Gordon <rossagordonstl@gmail.com>
2023-05-07 21:29:35 -0400

    flextopia logo fixed

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,8 +3,8 @@ import Flextopia from '../../src/assets/flextopia.jpg';
 
 const Logo = () => {
   return (
-    <div className='bg-[#000000] text-[#f5f5f7] w-full h-[50vh] flex justify-center items-center'>
-      <img src={Flextopia} className='w-[70%] h-[70%]' alt="Flextopia Records Logo" />
+    <div className='bg-[#000000] text-[#f5f5f7] w-full h-full flex justify-center items-center'>
+      <img src={Flextopia} className='w-[65%] h-[65%]' alt="Flextopia Records Logo" />
     </div>
   );
 };
