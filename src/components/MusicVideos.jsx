437a38690d974595d74399f24f50f6848603201f
Ross Gordon <rossagordonstl@gmail.com>
2023-05-10 11:17:26 -0400

    accessibility fixes

--- src/components/MusicVideos.jsx
+++ src/components/MusicVideos.jsx
@@ -3,8 +3,12 @@ import React from 'react';
 const MusicVideos = () => {
   return (
     <div className="flex items-center justify-center bg-[#000000]">
-      <div className="w-full h-full md:h-[75%] md:w-[75%] relative" style={{ paddingBottom: '56.25%', paddingTop: '0' }}>
+      <div
+        className="w-full h-full md:h-[75%] md:w-[75%] relative"
+        style={{ paddingBottom: '56.25%', paddingTop: '0' }}
+      >
         <iframe
+          title="Skinny V - Do Dat"
           className="absolute top-0 left-0 w-full h-full"
           src="https://www.youtube.com/embed/6caGGHI8jNI?start=9&controls=0&showinfo=0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
