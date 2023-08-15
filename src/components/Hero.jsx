8924d7ebb215db5ae0bb4afc2819bb2a921e501b
Ross Gordon <rossagordonstl@gmail.com>
2023-08-15 13:36:06 -0400

    fix Hero responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,27 +13,37 @@ const Hero = () => {
         {/* overlay */}
         <div className="absolute inset-0 bg-gray-900 opacity-50 sm:px-4 md:px-8 lg:px-16"></div>
       </div>
-      <div className="relative z-10 grid grid-cols-1 md:grid-cols-2 h-full px-4 sm:px-6 lg:px-8">
+      <div className="relative z-10 flex flex-col md:flex-row h-full px-4 sm:px-6 lg:px-8 items-center justify-center">
         {/* img container */}
-        <div className="flex justify-center items-center md:absolute md:left-1/4 md:top-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2">
+        <div className="flex justify-center items-center md:w-1/2 h-full">
           <img
             src={svFront}
             alt="SV Front"
-            className="object-cover object-center md:mb-0 sm:object-cover sm:object-center h-64 w-64 lg:h-96 lg:w-96 scale-125 lg:scale-150 "
+            className="object-cover object-center h-64 w-64 lg:h-96 lg:w-96 scale-125 lg:scale-150 sm:mb-8 md:mb-0"
           />
         </div>
-        <div className="flex flex-col items-center justify-center md:absolute md:left-3/4 md:top-1/2 md:transform md:-translate-x-1/2 lg:translate-y-[100px]">
-          <a href="https://music.apple.com/us/artist/skinny-v/1559103897" target="_blank" rel="noopener noreferrer" className="hidden sm:block">
-            <button
-              id="al"
-              aria-label="Listen Now"
-              className="bg-[#f5f5f7] hover:bg-[#0a0908] hover:text-[#f5f5f7] hover:border border-white transform -translate-y-[130px] text-[#0a0908] font-bold text-2xl py-6 px-16 rounded-lg focus:outline-none focus:shadow-outline"
-            >
+        <div className="flex flex-col items-center justify-center md:w-1/2 h-full">
+          <a
+            href="https://music.apple.com/us/artist/skinny-v/1559103897"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="sm:hidden sm:translate-y-150"
+          >
+            <button className="bg-[#f5f5f7] text-[#0a0908] font-bold text-2xl py-6 px-16 rounded-lg focus:outline-none focus:shadow-outline">
               Listen Now
             </button>
           </a>
-          <a href="https://music.apple.com/us/artist/skinny-v/1559103897" target="_blank" rel="noopener noreferrer" className="sm:hidden">
-            <button className="bg-[#f5f5f7] transform -translate-y-[140px] text-[#0a0908] font-bold text-2xl py-6 px-16 rounded-lg focus:outline-none focus:shadow-outline">
+          <a
+            href="https://music.apple.com/us/artist/skinny-v/1559103897"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hidden sm:block"
+          >
+            <button
+              id="al"
+              aria-label="Listen Now"
+              className="bg-[#f5f5f7] hover:bg-[#0a0908] hover:text-[#f5f5f7] hover:border border-white text-[#0a0908] font-bold text-2xl py-6 px-16 rounded-lg focus:outline-none focus:shadow-outline"
+            >
               Listen Now
             </button>
           </a>
