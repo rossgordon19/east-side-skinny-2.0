671ae05cad59f11d0e62e3cd41e1f83287ca04eb
Ross Gordon <rossagordonstl@gmail.com>
2023-08-15 13:41:53 -0400

    fix responsivness in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,23 +13,23 @@ const Hero = () => {
         {/* overlay */}
         <div className="absolute inset-0 bg-gray-900 opacity-50 sm:px-4 md:px-8 lg:px-16"></div>
       </div>
-      <div className="relative z-10 flex flex-col md:flex-row h-full px-4 sm:px-6 lg:px-8 items-center justify-center">
+      <div className="relative z-10 flex flex-col sm:flex-row h-full px-4 sm:px-6 lg:px-8 items-center justify-center">
         {/* img container */}
-        <div className="flex justify-center items-center md:w-1/2 h-full">
+        <div className="flex justify-center items-center sm:w-1/2 h-full">
           <img
             src={svFront}
             alt="SV Front"
-            className="object-cover object-center h-64 w-64 lg:h-96 lg:w-96 scale-125 lg:scale-150 sm:mb-8 md:mb-0"
+            className="object-cover object-center h-64 w-64 lg:h-96 lg:w-96 scale-125 lg:scale-150 sm:mb-0 md:mb-0"
           />
         </div>
-        <div className="flex flex-col items-center justify-center md:w-1/2 h-full">
+        <div className="flex flex-col items-center justify-center sm:w-1/2 h-full">
           <a
             href="https://music.apple.com/us/artist/skinny-v/1559103897"
             target="_blank"
             rel="noopener noreferrer"
-            className="sm:hidden sm:translate-y-150"
+            className="sm:hidden"
           >
-            <button className="bg-[#f5f5f7] text-[#0a0908] font-bold text-2xl py-6 px-16 rounded-lg focus:outline-none focus:shadow-outline">
+            <button className="bg-[#f5f5f7] text-[#0a0908] font-bold text-2xl py-6 px-16 rounded-lg focus:outline-none focus:shadow-outline sm:translate-y-150">
               Listen Now
             </button>
           </a>
@@ -37,7 +37,7 @@ const Hero = () => {
             href="https://music.apple.com/us/artist/skinny-v/1559103897"
             target="_blank"
             rel="noopener noreferrer"
-            className="hidden sm:block"
+            className="hidden sm:flex sm:items-center sm:justify-center"
           >
             <button
               id="al"
